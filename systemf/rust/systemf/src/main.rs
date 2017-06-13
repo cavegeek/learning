549d0c654e19819163c7d807134570f08ec36e2f
Jason Stumpf <jason.stumpf@gmail.com>
2017-06-13 16:24:36 -0700

    Added documentation.

--- systemf/rust/systemf/src/main.rs
+++ systemf/rust/systemf/src/main.rs
@@ -1,16 +1,29 @@
+//! A simple implementation of System F.
+
+/// A type expression.
+/// System F differentiates types (TypeExpr) from terms (Expr).
 #[derive(Debug)]
 enum TypeExpr {
+  /// A type variable.
   Id(String),
+  /// A function type (a -> b).
   Arrow(Box<TypeExpr>, Box<TypeExpr>),
+  /// A forall, (forall a b).
   ForAll(String, Box<TypeExpr>)
 }
 
+/// A term expression.
 #[derive(Debug)]
 enum Expr {
+  /// A term variable.
   Id(String),
+  /// Applying one term to another.
   Apply(Box<Expr>, Box<Expr>),
+  /// A lambda.
   Abstract(String, TypeExpr, Box<Expr>),
+  /// Applying a term to a type.
   TypeApply(Box<Expr>, TypeExpr),
+  /// A type lambda.
   TypeAbstract(String, Box<Expr>)
 }
 
