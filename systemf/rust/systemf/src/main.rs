4434345278070931f9eeeb017b942889ad4dd553
Jason Stumpf <jason.stumpf@gmail.com>
2017-06-13 16:02:08 -0700

    Added the enums describing System F.

--- systemf/rust/systemf/src/main.rs
+++ systemf/rust/systemf/src/main.rs
@@ -1,3 +1,19 @@
+#[derive(Debug)]
+enum TypeExpr {
+  Id(String),
+  Arrow(Box<TypeExpr>, Box<TypeExpr>),
+  ForAll(String, Box<TypeExpr>)
+}
+
+#[derive(Debug)]
+enum Expr {
+  Id(String),
+  Apply(Box<Expr>, Box<Expr>),
+  Abstract(String, TypeExpr, Box<Expr>),
+  TypeApply(Box<Expr>, TypeExpr),
+  TypeAbstract(String, Box<Expr>)
+}
+
 fn main() {
-    println!("Hello, world!");
+  println!("{:?}", Expr::TypeAbstract(String::from("a"), Box::new(Expr::Abstract(String::from("x"), TypeExpr::Id(String::from("a")), Box::new(Expr::Id(String::from("x")))))));
 }
