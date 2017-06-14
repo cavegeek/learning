084b1cd38e5e1279fbbdc13ee31f2bd597222ee0
Jason Stumpf <jason.stumpf@gmail.com>
2017-06-14 15:16:19 -0700

    Improved parser to parse application.

--- systemf/rust/systemf/src/main.rs
+++ systemf/rust/systemf/src/main.rs
@@ -28,17 +28,38 @@ enum Expr {
 }
 
 extern crate combine;
-use combine::{many, Parser, State};
-use combine::char::letter;
+use combine::{many1, parser, sep_by, skip_many1};
+use combine::{Parser, ParseResult, Stream};
+use combine::char::{letter, space};
+
+/// A parse function for Expr.
+/// This is needed because combine can't define recursive parsers without it.
+fn expr<I>(input: I) -> ParseResult<Expr, I>
+  where I: Stream<Item=char>
+{
+  let identifier = || many1(letter()).map(Expr::Id);
+  let subexpr = || identifier();
+  let white = || skip_many1(space());
+  let subexpr_seq = sep_by(subexpr(), white());
+  fn to_apply(e : Expr, l : Vec<Expr>) -> Expr {
+    let mut result = e;
+    for next in l {
+      result = Expr::Apply(Box::new(result), Box::new(next));
+    }
+    result
+  }
+  let mut apply = (subexpr(), white(), subexpr_seq).map(|(e,_,l)| to_apply(e,l));
+  apply.parse_stream(input)
+}
 
 // Parses its parameter, returning the corresponding expression or None.
 fn parse(input: &str) -> Option<Expr> {
-  match many(letter()).parse(input) {
-    Ok((id, "")) => Some(Expr::Id(id)),
+  match parser(expr).parse(input) {
+    Ok((e, "")) => Some(e),
     _ => None,
   }
 }
 
 fn main() {
-  println!("{:?}", parse("varname"));
+  println!("{:?}", parse("f x y"));
 }
